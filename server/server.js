7f923222231fea2dfa15966e51208bc915352d55
Resat Efe <proreastpro@gmail.com>
2025-11-29 21:18:02 +0300

    update server.js

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ app.use(express.json());
 const db = mysql.createConnection({
   host: process.env.DB_HOST || "localhost",
   user: process.env.DB_USER || "root",
-  password: process.env.DB_PASSWORD || "selAm07rst+",
+  password: process.env.DB_PASSWORD || "qqqqqqqq",
   database: process.env.DB_NAME || "tazekuru_db",
   port: process.env.DB_PORT || 3306
 });
